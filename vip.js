c85839436b67ea62ce3f38b5afd594cd2ec1b8d5
RodrigoFlores <rodrigoflores29061997@gmail.com>
2026-05-11 17:34:40 -0600

    third commit

---
+++ vip.js
@@ -0,0 +1,9 @@
+export default function vip(Total,cantidad){
+    if(cantidad>20 && Total>1000){
+            console.log("eres vip");
+    }
+    else {
+        console.log("No eres vip");
+    }
+
+}
